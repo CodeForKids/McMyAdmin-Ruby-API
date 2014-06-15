8ca19e5dc84e9edb0990536d46f5d8371918342d
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-06-15 01:03:41 -0400

    Use cmd arguments in test file

--- lib/test.rb
+++ lib/test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 $: << "."
 require 'McMyAdminAPI'
-c = CodeForKids::McMyAdminAPI.new('minecraft.codeforkids.ca','','')
+c = CodeForKids::McMyAdminAPI.new('minecraft.codeforkids.ca', ARGV[0], ARGV[1])
 
 # Put commands here!
