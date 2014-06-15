14d68b255484a99afb9cba7a884f668e2e9c6de9
Lucas <lucasmarcelli33@gmail.com>
2014-06-14 22:31:47 -0400

    Added addgroupvalue

--- code_for_kids/mc_my_admin_api.rb
+++ code_for_kids/mc_my_admin_api.rb
@@ -8,8 +8,11 @@ class CodeForKids
   class McMyAdminAPI
     sessionID = ""
 
-    def initialize(user = 'USERNAME', pass = 'PASSWORD')
-      url = URI.parse("http://minecraft.codeforkids.ca:8080/data.json?Username=#{user}&Password=#{pass}&Token=&req=login&MCMASESSIONID=")
+    #initializes the connection. Sets the sessionID
+    #host and port will be arguments for every function
+
+    def initialize(host, user, pass, port = '8080')
+      url = URI.parse("#{host}:#{port}/data.json?Username=#{user}&Password=#{pass}&Token=&req=login&MCMASESSIONID=")
       http = Net::HTTP.new url.host, url.port
       response = http.get("#{url.path}?#{url.query.to_s}", {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
       data = response.body
@@ -27,7 +30,41 @@ class CodeForKids
 
     end
 
+    #AddGroupValue
+    # Arguments:
+    ##########
+    # group - The group affected - string
+    ##########
+    # type - controls which info is changed - string
+    # type takes one of the following:
+    # groupslist (No group needs to be specified, used to add new groups)
+    # groupmembers (Add the user in 'value' as a member)
+    # groupcommands (Add the command in 'value' as a member)
+    # Color
+    # Inherits
+    # CanBuild
+    # CanInteract
+    # Prefix
+    # Suffix
+    ##########
+    # value - the value to be set by the method
+    # CanBuild and CanInteract take boolean
+    # the rest take string
+
+    def add_group_value(host, port, group, type, value)
+      url = URI.parse("#{host}:#{port}/data.json?req=addgroupvalue&group=#{group}&type=#{type}&value=#{value}")
+      http = Net::HTTP.new url.host, url.port
+      response = http.get("#{url.path}?#{url.query.to_s}", {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+      data = response.body
+
+      when Net::HTTPSuccess
+        #worked
+      else
+        # response code isn't a 200; raise an exception
+        response.error!
+      end
   end
 end
 
-CodeForKids::McMyAdminAPI.new()
+cfk_api = CodeForKids::McMyAdminAPI.new('host', 'username', 'password')
+cfk_api.getStatus()
